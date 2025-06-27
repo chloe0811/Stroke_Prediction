afc075cb793865bcdd1c6974ec60f972d572fc28
chloe0811 <167526055+chloe0811@users.noreply.github.com>
2025-06-27 21:06:58 +1000

    update code

--- data.py
+++ data.py
@@ -8,9 +8,6 @@ def get_clean_stroke_data(local_save=True):
     stroke_data = os.path.join(path, "healthcare-dataset-stroke-data.csv")
     df = pd.read_csv(stroke_data)
 
-    # Fill missing values
-    df['bmi'] = df['bmi'].fillna(df['bmi'].mean())
-
     # Optional: Save locally
     if local_save:
         output_path = "/Users/sptsai/Documents/GitHub/Stroke_Prediction/stroke_data.csv"
